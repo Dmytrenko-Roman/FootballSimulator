a814f700d647eac34461d34a31a6506e9defb12c
Dmytrenko-Roman <54878089+Dmytrenko-Roman@users.noreply.github.com>
2020-07-14 19:37:35 -1100

    MU added.

---
+++ main/team.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Team {
+  string name;
+  string city;
+  int players;
+  int[] rating;
+
+  public Team(string n, string c, int p, int[] r) {
+    name = n;
+    city = c;
+    players = p;
+    rating = r;
+  } 
+  public int AverageRating() {
+    int sum = 0;
+    for (int i = 0; i < rating.Length; i++) {
+      sum += rating[i];
+    }
+    return sum / players;
+  }
+}
